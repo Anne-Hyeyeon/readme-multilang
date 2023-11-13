d852806d6aa7803d2265eda9110d2c40ee8c31e9
Hyeyeon(Anne) <khy2106@naver.com>
2023-11-13 15:54:24 +0900

    Update getimage.ts

--- api/getimage.ts
+++ api/getimage.ts
@@ -8,8 +8,6 @@ export default function(req: VercelRequest, res: VercelResponse) {
   ];
 
 const randomJson:  {title: string} = jsons[Math.floor(Math.random() * jsons.length)];
-const timestamp: number = new Date().getTime();
-
 
 const svgContent = `
     <svg width="200" height="200" xmlns="http://www.w3.org/2000/svg">
@@ -18,5 +16,5 @@ const svgContent = `
   `;
 
   res.setHeader('Content-Type', 'image/svg+xml');
-  res.status(200).send(svgContent + `?t=${timestamp}`);
+  res.status(200).send(svgContent);
 }
