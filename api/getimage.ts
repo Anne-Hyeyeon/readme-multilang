1acf7cc4d9ea489c0e3b810ce1c39751ebcba309
Hyeyeon(Anne) <khy2106@naver.com>
2023-11-13 15:19:23 +0900

    Update getimage.ts

--- api/getimage.ts
+++ api/getimage.ts
@@ -1,10 +1,13 @@
-module.exports = (req, res) => {
-  const images = [
-    'https://raw.githubusercontent.com/Anne-Hyeyeon/mystorage/main/multilang/github_multilang_en.png',
-    'https://raw.githubusercontent.com/Anne-Hyeyeon/mystorage/main/multilang/github_multilang_ko.png',
+import type { VercelRequest, VercelResponse } from '@vercel/node';
+
+export default function(req: VercelRequest, res: VercelResponse) {
+  const images: string[] = [
+    'https://github.com/Anne-Hyeyeon/mystorage/blob/main/multilang/github_multilang_ko.png?raw=true',
+    'https://github.com/Anne-Hyeyeon/mystorage/blob/main/multilang/github_multilang_en.png?raw=true',
+    'https://example.com/image3.jpg',
   ];
 
-  const randomImage = images[Math.floor(Math.random() * images.length)];
+  const randomImage: string = images[Math.floor(Math.random() * images.length)];
 
-  res.status(200).json({ imageUrl: randomImage });
-};
+  res.redirect(randomImage);
+}
