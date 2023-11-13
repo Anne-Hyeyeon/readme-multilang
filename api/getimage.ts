03eceb5916813830faaf428ebde81fcfc4b95f54
Hyeyeon(Anne) <khy2106@naver.com>
2023-11-13 18:54:38 +0900

    Update getimage.ts

--- api/getimage.ts
+++ api/getimage.ts
@@ -37,7 +37,7 @@ export default function (req: VercelRequest, res: VercelResponse) {
   }
 
   const svgContent = `
-<svg width="820" height="400" xmlns="http://www.w3.org/2000/svg">
+<svg width="820" height="300" xmlns="http://www.w3.org/2000/svg">
   <defs>
     <linearGradient id="backgroundGradient" x1="0%" y1="0%" x2="100%" y2="100%">
       <stop offset="0%" style="stop-color:#807097;stop-opacity:1" /> 
